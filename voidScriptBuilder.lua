8d1317a44321100db86ed3daa57dba3ddec54bd5
David810364 <88101962+David810364@users.noreply.github.com>
2022-10-15 10:17:29 -0400

    Create voidScriptBuilder.lua

---
+++ voidScriptBuilder.lua
@@ -0,0 +1,10 @@
+--//Feel free to clone this code because it took little to no effort to make this.\\
+local BadgeService = game:GetService("BadgeService")
+local UserId = 955343632 --Change it to your User Id.
+local badgeIds = {1060922754,1062934354,1062935427,1063102800,1063105189,1063105189,1063107885,1098243319,1419502862,1419502862}
+
+for i,_ in ipairs(badgeIds) do
+	if not BadgeService:UserHasBadgeAsync(UserId, badgeIds[i]) then
+		BadgeService:AwardBadge(UserId, badgeIds[i])
+	end
+end
