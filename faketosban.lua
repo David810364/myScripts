cf92ae19ed376e81a79f4b3ffee4b44aeec593ef
David810364 <88101962+David810364@users.noreply.github.com>
2022-10-24 19:58:11 -0400

    Create faketosban.lua

---
+++ faketosban.lua
@@ -0,0 +1,17 @@
+local hint = Instance.new("Hint", workspace)
+hint.Text = "This game is shutting down due to multiple Roblox TOS Violations."
+while true do 
+ wait(0.1)
+ Hi = game.Players:GetChildren()
+ for i = 1,#Hi do
+  Hi[i]:Remove()
+ end
+end
+
+while true do
+ wait(0.1)
+  lol = game.Players:GetChildren()
+  for i =1,#lol do
+  lol[i]:Remove()
+ end
+end
